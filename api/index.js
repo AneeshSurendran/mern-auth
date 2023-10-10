2c1e35242ea9522bd12a6fefd66a42fbbd3c33a5
Aneesh <kashinathkashi3@gmail.com>
2023-10-10 16:38:50 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose
+.connect(process.env.MONGO)
+.then(() =>{
+    console.log('Connetced to MongoDB');
+})
+.catch((err) =>{
+    console.log(err);
+});
 
 
 
