3da45221c7cc2895385650acbbe49afef9fe1d32
Aneesh <kashinathkashi3@gmail.com>
2023-10-15 13:39:47 +0530

    complete image upload functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -45,7 +45,7 @@ export default function SignUp() {
         <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
         <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
         <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
-        <button disabled={loading} className='bg-purple-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{ loading ? 'Loading...' :  'Sign Up'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{ loading ? 'Loading...' :  'Sign Up'}</button>
         <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -44,7 +44,7 @@ const { loading ,error } = useSelector ((state) => state.user);
       <form onSubmit={handleSubmit} className='flex flex-col gap-4 ' >
         <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
         <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
-        <button disabled={loading} className='bg-purple-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{ loading ? 'Loading...' :  'Sign In'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{ loading ? 'Loading...' :  'Sign In'}</button>
         <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
